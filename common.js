b78b92f4c8f5f19d2a543f6dbfb528037a4cca9b
Renegatxxx <157229742+Renegatxxx@users.noreply.github.com>
2024-02-25 10:15:55 +0200

    Create common.js

---
+++ common.js
@@ -0,0 +1,13 @@
+(function(){
+var er = document.querySelector(".header-navigation");
+     var tr = document.querySelector(".main-nav__inner");
+     var n = document.querySelector(".main-nav__button");
+     var i = document.querySelector(".main-nav__button-line");
+
+
+
+n.addEventListener("click", function(event){
+  tr.classList.add("main-nav__inner--visible");
+ });
+
+        });
